6d99dc0d5e1a490900c782b5728111a230f5aa86
Jason Crawford <daxapps777@gmail.com>
2016-07-27 17:50:28 -0500

    Udemy

---
+++ MyBrainTeaser/MyBrainTeaser/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MyBrainTeaser
+//
+//  Created by Jason Crawford on 7/27/16.
+//  Copyright © 2016 Dax Apps. All rights reserved.
+//
+
+import UIKit
+import pop
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
